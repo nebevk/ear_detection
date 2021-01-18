ce06753b96bde82e823160a0b4547f8e1cebc163
nebevk <44230282+nebevk@users.noreply.github.com>
2021-01-18 10:35:55 +0100

    Update report and notebooks

--- train.py
+++ train.py
@@ -4,6 +4,23 @@ import os
 import cv2
 from PIL import Image
 
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import cv2
+import keras
+import pathlib
+import PIL
+from PIL import Image
+from imgaug import augmenters as iaa
+from keras.applications.xception import Xception
+from keras.models import Model
+import tensorflow as tf
+from tensorflow.keras import layers
+import tensorflow_datasets as tfds
+import pandas as pd
+from IPython.display import display
+
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 print(BASE_DIR)
 image_dir = os.path.join(BASE_DIR, "awe")
